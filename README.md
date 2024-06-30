8fd750907dcb9e5f575ddd99b639942987b41458
Aparna-xxx <aparnaramanathan14@gmail.com>
2024-06-30 09:39:12 +0530

    changed animation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ~ A skeleton code for having animated splash screens in react native (expo app)
 ~ uses lottie library
+
+~colours used
+
+loading screen animation - #FF8000
+loading screen background - #EEF5FF
--- animation/screens/AnimationScreen.js
+++ animation/screens/AnimationScreen.js
@@ -1,18 +1,50 @@
-import { View , StyleSheet} from 'react-native';
+import { View , StyleSheet,  Animated, Easing,} from 'react-native';
 import LottieView from 'lottie-react-native';
-
+import React, { useEffect, useRef } from 'react';
 
 
 function SplashScreen( ){
   
+  const moveAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(1)).current;
+
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(moveAnim, {
+        toValue: -100, // Adjust the value based on the screen size and your requirement
+        duration: 1000,
+        easing: Easing.inOut(Easing.ease),
+        useNativeDriver: true,
+      }),
+      Animated.timing(scaleAnim, {
+        toValue: 0.3,
+        duration: 1000,
+        easing: Easing.inOut(Easing.ease),
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, [moveAnim, scaleAnim]);
 
   
     return (
         <View style={styles.container}>
         <View style={styles.welcome}>
+        <Animated.View
+        style={[
+          styles.animatedView,
+          {
+            transform: [
+              { translateY: moveAnim },
+              { scale: scaleAnim },
+            ],
+          },
+        ]}
+      >
              <LottieView  autoPlay
                 loop={false}
-                style={{flex:1}}source={require('../assets/animations/chaiAnimation.json') }  />
+                style={styles.gifStyle}source={require('../assets/animations/loadFood.json') }  />
+      </Animated.View>
              </View>
         </View>
       );
@@ -25,12 +57,24 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       alignItems: 'center',
-      backgroundColor:'#A7C7E7',
+      backgroundColor:'#EEF5FF',
       justifyContent: 'center',
+      
     },
     welcome: {
       height:300,
       aspectRatio:1,
-    }
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    animatedView: {
+    width: 300,
+    height: 300,
+  
+  },
+  gifStyle: {
+    flex:1,
+    
+  }
   });
   
